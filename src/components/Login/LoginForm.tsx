3180b4ef4ea3c7728904711138248f50b0d44df3
skillforce@mail.ru <skillforce@mail.ru>
2021-09-11 21:05:20 +0300

    75+ (without type)

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
+import s from './LoginForm.module.css';
+
+
+const{inputField,checkBox,btnLog}=s;
 
 
 export const LoginForm = (props: any) => {
 
     return (<form onSubmit={props.handleSubmit}>
-            <div>
+            <div className={inputField}>
+            <div >
                 <Field placeholder={'Login'} name={'login'} type={'text'} component={'input'}/>
             </div>
             <div>
                 <Field placeholder={'Password'} name={'password'} component={'input'} type={'text'}/>
             </div>
-            <div>
+            </div>
+            <div className={checkBox}>
                 <Field name={'rememberMe'} component={'input'} type={'checkbox'}/>remember me
             </div>
-            <div>
-                <button onClick={props.onSubmit}>LOGIN IN!</button>
+            <div className={btnLog}>
+                <button onClick={props.onSubmit}>Log In</button>
             </div>
+
         </form>
     )
 }
--- src/components/Login/login.tsx
+++ src/components/Login/login.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import {ReduxLoginForm} from './LoginForm';
+import s from './login.module.css';
+import login1 from '../../img/log1.png';
+import login2 from '../../img/log2.png';
+import login3 from '../../img/log3.png';
+import login4 from '../../img/log4.png';
+
+
+
+const{loginPage,loginImg1,loginImg2,loginImg3,loginImg4}= s;
 
 
 export const Login = () => {
@@ -7,8 +16,12 @@ export const Login = () => {
         console.log(formData)
     }
 
-    return (<div>
-            <h1>Login</h1>
+    return (<div className = {loginPage}>
+            <div className = {loginImg1}><img src={login1} alt="loginPageImg1"/></div>
+            <div className = {loginImg2}><img src={login2} alt="loginPageImg2"/></div>
+            <div className = {loginImg3}><img src={login3} alt="loginPageImg3"/></div>
+            <div className = {loginImg4}><img src={login4} alt="loginPageImg3"/></div>
+            <span>Log In</span>
             <ReduxLoginForm onSubmit={onSubmit}/>
         </div>
     )
