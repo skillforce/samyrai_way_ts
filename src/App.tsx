5280cced3bbcbc2170ffb30ba83558f6601d13b4
skillforce <7401642Denis>
2021-11-03 15:24:01 +0300

    commit test1

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ const App: React.FC = () => {
         dispatch(getAuthMe(userId))
     }, [userId])
 
-//+_+
+
     if (!initialized) {
         return (<div className="app-wrapper">
             <HeaderContainer/>
